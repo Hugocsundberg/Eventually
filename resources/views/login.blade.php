60adf703acd9383dbfac56a2d1cf2b070f3c6576
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-02 19:05:57 +0200

    Add basic login view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+<x-app>
+    <div class="container center-center-col">
+        <p>Hello, this is login</p>
+        <form class="center-center-col" action="">
+            <Label for="name">Name</Label>
+            <input id="name" type="text">
+            <Label for="password">Password</Label>
+            <input id="password" type="password">
+        </form>
+    </div>
+</x-app>
