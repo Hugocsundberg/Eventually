c4a372644d40fb8bf93983adcf1f7f84e34b227d
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-03 00:55:46 +0200

    Minor style update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,13 @@
 <x-app>
     <div class="container center-center-col">
-        <p>Hello, this is login</p>
-        <form class="center-center-col" action="">
-            <Label for="name">Name</Label>
-            <input id="name" type="text">
-            <Label for="password">Password</Label>
-            <input id="password" type="password">
-        </form>
+        <div class="center-container center-center-col">
+            <p>Hello, this is login</p>
+            <form class="center-center-col" action="">
+                <Label for="name">Name</Label>
+                <input id="name" type="text">
+                <Label for="password">Password</Label>
+                <input id="password" type="password">
+            </form>
+        </div>
     </div>
 </x-app>
