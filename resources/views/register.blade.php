0e10cc55dc8636578f4b2df9c59b8aa7a4d770ca
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-04 14:18:21 +0200

    Add register view

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,16 +2,16 @@
     <div class="container center-center-col">
         <div class="center-container center-center-col">
             @include('errors')
-            <p>Hello, this is login</p>
-            <form class="center-center-col" method="post" action="/login">
+            <p>Hello, this is register</p>
+            <form class="center-center-col" method="post" action="/register">
                 @csrf
                 <Label for="email">Email</Label>
                 <input id="email" name="email" type="email">
                 <Label for="password">Password</Label>
                 <input id="password" name="password" type="password">
-                <input id="password" type="submit" value="Login">
+                <input id="password" type="submit" value="Register">
             </form>
-            <a href="/register">Register</a>
+            <a href="/login">Login</a>
         </div>
     </div>
 </x-app>
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 })->name('login');
+
+Route::post('/login', [LoginController::class, 'checkLogin']);
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::post('/register', [RegisterController::class]);
 
 Route::get('/welcome', function () {
     return view('welcome');
 })->middleware('auth');
-
-Route::post('/login', [LoginController::class, 'checkLogin']);
