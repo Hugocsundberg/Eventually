1593ae4cd954e11d4c4b039658144e8e86644a7e
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-07 14:52:08 +0200

    Update route

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -2,7 +2,7 @@
     <div class="left logo"><a class="nav-link" href="/">Eventually</a></div>
     <div class="right">
         <div class="nav-item-list ">
-            <a class="nav-link" href="/welcome">Home</a>
+            <a class="nav-link" href="/dashboard">Home</a>
             <a class="nav-link" href="/logout">Logout</a>
         </div>
     </div>
