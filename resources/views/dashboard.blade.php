436b5b026e149ff07162633c02ff8cdb627ccead
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-07 14:42:44 +0200

    Add dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+<x-app>
+    <div class="container center-center-col">
+        <div class="center-container center-center-col">
+            <a class="btn btn-primary" href="">Skapa evenemang</a>
+            <h1>Dina evenemang</h1>
+        </div>
+    </div>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 })->middleware('auth');
 
 Route::get('/login', function () {
