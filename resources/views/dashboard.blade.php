cf49b7c45a8a5c97cec299fe7d1b2dedcb5ef280
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-07 14:43:50 +0200

    Update route

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app>
     <div class="container center-center-col">
         <div class="center-container center-center-col">
-            <a class="btn btn-primary" href="">Skapa evenemang</a>
+            <a class="btn btn-primary" href="/create-event">Skapa evenemang</a>
             <h1>Dina evenemang</h1>
         </div>
     </div>
