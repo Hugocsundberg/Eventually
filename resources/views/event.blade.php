3d05d5a91e20aba7856bd3a5a7688717cdfeac13
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-04-08 14:53:54 +0200

    Update event.blade.php
    Fixed seperate date and time

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -21,13 +21,15 @@ $date // prints only date of event (yyyy-mm-dd)
         <div class="max-width">
             <section class="event_top-section  center-center-col">
                 <img src="https://media.routard.com/image/67/1/fb-canada-parcs.1473671.jpg" alt="">
-                <time class="date text-white">{{$event->event_date}}</time>
+                <time class="date text-white">{{$date}}</time>
                 <h1 class="title text-white">{{$event->event_name}}</h1>
             </section class="event_mid-section">
             <section class="mid-section center-center-col">
                 <div class="card-background padding">
                     <h4 class="">Datum:</h4>
-                    <time>{{$event->event_date}}</time>
+                    <time>{{$date}}</time>
+                    <h4>Time:</h4>
+                    <time>{{$time}}</time>
                     <h4 class="">Beskrivning</h4>
                     <p class="description ">{{$event->event_description}}</p>
                 </div>
