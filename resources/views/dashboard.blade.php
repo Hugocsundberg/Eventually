a795e36dcb90fde63800a2ca4a11684f3434d3e2
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-08 14:51:29 +0200

    Fix dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,11 @@
         <div class="center-container center-center-col">
             <a class="btn btn-primary" href="/create-event">Create event</a>
             <h1>Your events</h1>
+            @foreach ($events as $event)
+            <a href="/event-page/{{$event->event_id}}">
+                <h1>{{$event->event_name}}</h1>
+            </a>
+            @endforeach
         </div>
     </div>
 </x-app>
