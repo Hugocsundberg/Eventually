085f530da4da0c10798ec6e5f5318fdd587c38d7
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-04-08 13:22:51 +0200

    Update event.blade.php

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -4,15 +4,15 @@
         <div class="max-width">
             <section class="event_top-section  center-center-col">
                 <img src="https://media.routard.com/image/67/1/fb-canada-parcs.1473671.jpg" alt="">
-                <p class="date text-white">7 apr 2021</p>
-                <h1 class="title text-white">Evenemanget</h1>
+                <time class="date text-white">{{$event->event_date}}</time>
+                <h1 class="title text-white">{{$event->event_name}}</h1>
             </section class="event_mid-section">
             <section class="mid-section center-center-col">
                 <div class="card-background padding">
                     <h4 class="">Datum:</h4>
-                    <p>7 apr 2021</p>
+                    <time>{{$event->event_date}}</time>
                     <h4 class="">Beskrivning</h4>
-                    <p class="description ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure ratione inventore porro iste ipsum suscipit a quae. Amet, fugiat cumque accusantium debitis earum iusto nemo cum. Voluptatem dignissimos rem optio!</p>
+                    <p class="description ">{{$event->event_description}}</p>
                 </div>
             </section>
         </div>
