2efd99c6f2d793ef5385fe27ff8423ea8fa16100
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-14 15:52:20 +0200

    Add go to edit test

--- resources/views/edit-event.blade.php
+++ resources/views/edit-event.blade.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // die(var_dump($event));
 $timePos = strrpos($event->event_date, 'T');
 $time = substr($event->event_date, $timePos);
@@ -6,7 +6,7 @@ $date = str_replace($time, '', $event->event_date);
 $time = str_replace('T', '', $time);
 $user = Auth::user();
 // die(var_dump($user));
-if(isset($user->id) && $user->id === $event->event_host){
+if (isset($user->id) && $user->id === $event->event_host) {
     $host = $user;
 }
 
@@ -27,17 +27,17 @@ $date // prints only date of event (yyyy-mm-dd)
             <form class="event_edit" action='/event-page/{{$event->event_id}}/edit-event' method='post'>
                 @csrf
 
-                <input type="hidden" id="event_name" name="event_name"/>
-                <input type="hidden" id="event_location" name="event_location"/>
-                <input type="hidden" id="event_date" name="event_date"/>
-                <input type="hidden" id="event_description" name="event_description"/>
+                <input type="hidden" id="event_name" name="event_name" />
+                <input type="hidden" id="event_location" name="event_location" />
+                <input type="hidden" id="event_date" name="event_date" />
+                <input type="hidden" id="event_description" name="event_description" />
                 <input type="hidden" id="event_id" name="event_id" value="{{$event->event_id}}">
 
                 <section class="event_top-section  center-center-col">
                     <img src="https://media.routard.com/image/67/1/fb-canada-parcs.1473671.jpg" alt="">
                     <time class="date text-white">{{$date}}</time>
                     <h1 contenteditable="true" class="title text-white">{{$event->event_name}}</h1>
-                    
+
                 </section class="event_mid-section">
                 <section class="mid-section center-center-col">
                     <div class="card-background padding">
@@ -51,25 +51,25 @@ $date // prints only date of event (yyyy-mm-dd)
                         <p contenteditable="true" class="description ">{{$event->event_description}}</p>
                     </div>
                 </section>
-                
-                
+
+
                 @foreach ($comments as $comment)
-                    <div class="card-background padding">
-                        @if($comment->from_host != null)
-                            <strong>{{$comment->from_host}}</strong>
-                        @endif
-                        <p>
-                            "{{$comment->message}}"
-                        </p>
-                        @if (isset($host))
-                            <a href='#' class='btn btn-primary' data-comment="{{$comment->id}}" id="delete_btn">DELETE COMMENT</a>
-                        @endif
-                    </div>
+                <div class="card-background padding">
+                    @if($comment->from_host != null)
+                    <strong>{{$comment->from_host}}</strong>
+                    @endif
+                    <p>
+                        "{{$comment->message}}"
+                    </p>
+                    @if (isset($host))
+                    <a href='#' class='btn btn-primary' data-comment="{{$comment->id}}" id="delete_btn">DELETE COMMENT</a>
+                    @endif
+                </div>
                 @endforeach
                 @if (isset($host))
-                    <button id="save" class="btn btn-primary" type=button>Save</button>
-                    <button id="submit" style="display: none">Submit</button>
-                    <a href='#' class='btn btn-primary' id="delete_btn">DELETE EVENT</a>
+                <button id="save" class="btn btn-primary" type=button>Save</button>
+                <button id="submit" style="display: none">Submit</button>
+                <a href='#' class='btn btn-primary' id="delete_btn">DELETE EVENT</a>
                 @endif
             </form>
         </div>
