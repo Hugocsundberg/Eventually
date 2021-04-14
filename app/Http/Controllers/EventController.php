f2bed1abda433a0325e1307a42e876f4a85c4f5d
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-04-14 14:14:12 +0200

    Fix redirect to dashboard on event removal

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -27,7 +27,7 @@ class EventController extends Controller
     {
         DB::table('events')->where('event_id', '=', $event_id)->delete();
         DB::table('guestbooks')->where('event_id', '=', $event_id)->delete();
-        return redirect()->intended('/dashboard');
+        return redirect('/dashboard');
     }
 
     public function addComment(Request $request)
@@ -92,6 +92,6 @@ class EventController extends Controller
 
     public function saveChanges(Request $request)
     {
-        die(var_dump($request['event_name']));
+        dd($request);
     }
 }
